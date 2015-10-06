4f8ff238e4a300bff7ac0471d5843493cfd00af6
Eirik Lavik <eiriklavik@gmail.com>
2015-10-06 10:57:47 +0200

    renameing

---
+++ python-text-game.py
@@ -0,0 +1,37 @@
+# Amma make a game!
+import function
+import variables
+
+
+# Title
+print "This is a game"
+print " "
+playerName = raw_input("What is your name? ")
+print "Welcome to this world, %s." % playerName
+print " "
+
+next()
+print " "
+print "This is a world of chaos. Infested with orcs. It is your quest to stop them."
+print " "
+
+next()
+
+print " "
+print "Here is your initial stats:"
+playerStat()
+
+print " "
+print "Good luck."
+
+next()
+
+thing = "shiny metal object"
+
+print " "
+print "You look out into the wilderness that is this world."
+print "You see a %s on the ground." % thing
+print " "
+
+select()
+
