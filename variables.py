eac26714324f2254b1344d749ba39d9415a1b01e
John ShaggyTwoDope Jenkins <twodopeshaggy@gmail.com>
2015-10-06 02:48:18 -0700

    some adjustments

--- variables.py
+++ variables.py
@@ -1,9 +1,17 @@
 # These are the main variables for the game
 
 # Names and initial stats
-
+global playerHealth
 playerHealth	= 100
+
+global playerDamage
 playerDamage	= 10
+
+global playerArmor
 playerArmor	= 0
+
+global playerWeapon
 playerWeapon	= "none"
+
+global thing
 thing		= "none"
